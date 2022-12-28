971d334a15a24eb8a8585ef9633a415f873eb6c1
huseynManaf <huseynim@code.edu.az>
2022-12-28 04:14:26 -0800

    init

---
+++ ConsoleApp51/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp51
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ededi daxil edin:");
+            int n, r2, r4, r6;
+            n = int.Parse(Console.ReadLine());
+
+            r2 = (n / 10000) % 10;
+            r4 = (n / 100) % 10;
+            r6 = n % 10;
+            double pow_r2 = Math.Pow(r2, 2);
+            double pow_r4 = Math.Pow(r4, 2);
+            double pow_r6 = Math.Pow(r6, 2);
+            double sum = pow_r2 + pow_r4 + pow_r6;
+            Console.WriteLine(sum);
+        }
+    }
+}
